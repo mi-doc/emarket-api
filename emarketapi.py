2846301eb8b25e19355a699a702213195bbc2474
Mikhail Nikolaev <m.nikolaev1@gmail.com>
2019-12-15 16:44:30 +0300

    Implementation without context managers

--- emarketapi.py
+++ emarketapi.py
@@ -1,42 +1,21 @@
 import asyncio
 import aiohttp
-from contextlib import closing
 # import sys
-# import requests
 from prettytable import PrettyTable
 
 
 URL = 'https://emarket-shop.herokuapp.com/api/products-list'
 
-# print('wait... \n')
-# r = requests.get(URL)
-# products = r.json()
-#
+
 # if len(sys.argv) > 1:
 #     keys = sys.argv[1:]
 # else:
 #     keys = ['name']
-#
-# table = PrettyTable(keys)
-# for product in products:
-#     vals = []
-#     for key in keys:
-#         val = product[key]
-#         vals.append(val)
-#     table.add_row(vals)
-# print(table)
-
-
-# sync def timer(sec):
-# for s in range(sec):
-# 	print(s)
-# 	await asyncio.sleep(1)
 
 
 def prettyprint(products, keys):
 	table = PrettyTable(keys)
 	for product in products:
-		print(product)
 		vals = []
 		for key in keys:
 			val = product[key]
@@ -45,13 +24,11 @@ def prettyprint(products, keys):
 	print(table)
 
 
-async def fetch_product_list(loop, keys=['name'], url=URL):
-	with aiohttp.ClientSession(loop=loop) as session:
-		with aiohttp.Timeout(31):
-			async with session.get(url) as r:
-				assert r.status == 200
-				products = await r.json()
-				prettyprint(products, keys)
+async def fetch_product_list(keys=['name'], url=URL):
+	r = await aiohttp.request('GET', URL)
+	products = await r.json()
+	r.close()
+	prettyprint(products, keys)
 
 
 async def timer(sec):
@@ -61,8 +38,8 @@ async def timer(sec):
 
 
 def run ():
-	with closing(asyncio.get_event_loop()) as loop:
-		loop.run_until_complete(asyncio.gather(fetch_product_list(loop), timer(3)))
+	loop = asyncio.get_event_loop()
+	loop.run_until_complete(asyncio.gather(fetch_product_list(), timer(3)))
 	print('Done')
 
 
